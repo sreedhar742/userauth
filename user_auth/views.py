5f6b4ba103ce9409a06a1a5d96cc769a4b8376b1
sreedhar742 <psridhar742@gmail.com>
2024-09-30 20:05:44 +0530

    delete some files

--- user_auth/views.py
+++ user_auth/views.py
@@ -28,9 +28,7 @@ def login_view(request):
             return redirect('login')
     return render(request, 'login.html')
 # views.py
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .models import UserProfile
+
 
 # views.py
 from django.shortcuts import render, get_object_or_404
